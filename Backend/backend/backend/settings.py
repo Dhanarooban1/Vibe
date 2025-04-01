07ba2d839bfff22789ad7309806fdab39bb15872
Dhanarooban1 <dhanazx1@gmail.com>
2025-04-01 10:06:59 +0530

    Making changes in settings.py file

--- Backend/backend/backend/settings.py
+++ Backend/backend/backend/settings.py
@@ -13,9 +13,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-^mj$-dslbg428h&yrml*sk=jkd^#uh561epwm6-&)+s&w^q_fq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+# DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'https://vibe-6ewf.vercel.app/').split(',')
+
 
 # Application definition
 INSTALLED_APPS = [
